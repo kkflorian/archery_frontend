1edd20fe3bc24e13ed9c54bed9199e1d77dfe679
Florian <privat@florian.ws>
2020-09-25 23:02:08 +0200

    Update less loader config

--- craco.config.js
+++ craco.config.js
@@ -1,19 +1,20 @@
 const CracoLessPlugin = require('craco-less');
-
 module.exports = {
     plugins: [
         {
             plugin: CracoLessPlugin,
             options: {
-                lessLoaderOptions: {
-                    lessOptions: {
-                        modifyVars: {
-                            '@primary-color': '#1DA57A'
-                        },
-                        javascriptEnabled: true,
-                    },
+                modifyLessRule: function(lessRule, _context) {
+                    lessRule.test = /(\.(module))?\.(less)$/;
+                    lessRule.exclude = /node_modules/;
+                    return lessRule;
                 },
-            },
-        },
-    ],
+                cssLoaderOptions: {
+                    modules: {
+                        localIdentName: "[local]_[hash:base64:5]"
+                    }
+                }
+            }
+        }
+    ]
 };
