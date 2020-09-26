e92f1681cb93911ffcbe37e1b0574cb79d47c6cc
Florian <privat@florian.ws>
2020-09-26 18:20:39 +0200

    Remove create wrapper function

--- src/shared/misc.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export function createWrapperFunction(Component) {
-  return (children) => (<Component>{children}</Component>);
-}
--- src/views/LoginLayout/LoginLayout.js
+++ src/views/LoginLayout/LoginLayout.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {Layout} from "antd";
 import classes from "./LoginLayout.module.less";
-import {createWrapperFunction} from "../../shared/misc";
 
 const LoginLayout = ({children}) => {
   return (
@@ -14,5 +13,5 @@ const LoginLayout = ({children}) => {
 };
 
 export default Object.assign(LoginLayout, {
-  wrap: createWrapperFunction(LoginLayout)
+  wrap: (children) => (<LoginLayout>{children}</LoginLayout>)
 });
