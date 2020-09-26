4e8a9d3e5c4224235093ca033616e96a08352e1e
Florian <privat@florian.ws>
2020-09-26 19:03:19 +0200

    Add default validation rules

---
+++ src/shared/misc.js
@@ -0,0 +1,4 @@
+export const defaultRules = {
+  required: {required: true, message: "Dieses Feld wird benötigt"},
+  requiredNoWhitespace: {required: true, whitespace: true, message: "Dieses Feld wird benötigt"}
+};
