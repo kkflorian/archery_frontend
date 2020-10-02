7f4fbc4c9f18146e6033f4462cf0d6d1c047817c
Florian <privat@florian.ws>
2020-10-02 11:40:52 +0200

    Add basic authenticated pages

---
+++ src/views/AuthenticatedLayout/AuthenticatedLayout.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import cls from './AuthenticatedLayout.module.less';
+
+export default () => {
+
+}
---
+++ src/views/AuthenticatedLayout/HomePage/HomePage.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import cls from './HomePage.module.less';
+
+export default () => {
+
+}
