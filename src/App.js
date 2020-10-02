7ff54c33046b9fa16438b929919ecc594c8db240
Florian <privat@florian.ws>
2020-10-02 11:41:34 +0200

    Add basic authenticated area

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.less';
-import {BrowserRouter, Redirect, Switch} from "react-router-dom";
+import {Route, BrowserRouter, Redirect, Switch} from "react-router-dom";
 import TitledRoute from "./shared/TitledRoute";
 import LoginPage from "./views/LoginLayout/LoginPage/LoginPage";
 import RegisterPage from "./views/LoginLayout/RegisterPage/RegisterPage";
+import HomePage from "./views/AuthenticatedLayout/HomePage/HomePage";
 
 function App() {
   return (
@@ -12,10 +13,20 @@ function App() {
         <TitledRoute title="Anmeldung" path="/login" component={LoginPage}/>
         <TitledRoute title="Registrierung" path="/register" component={RegisterPage}/>
 
+        <AuthenticatedArea/>
+
         <Redirect to="/a" />
       </Switch>
     </BrowserRouter>
   );
+}
+
+function AuthenticatedArea() {
+    return (
+        <>
+            <TitledRoute title="Home" path="/home" component={HomePage} />
+        </>
+    )
 }
 
 export default App;
