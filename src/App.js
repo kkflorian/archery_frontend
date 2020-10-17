c60f92a22d5a67c3a2a9ad66d39bd11e7f787246
Florian <privat@florian.ws>
2020-10-17 20:34:30 +0200

    Add user context; Update api client

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import TitledRoute from "./shared/TitledRoute";
 import LoginPage from "./views/LoginLayout/LoginPage/LoginPage";
 import RegisterPage from "./views/LoginLayout/RegisterPage/RegisterPage";
 import HomePage from "./views/AuthenticatedLayout/HomePage/HomePage";
+import {api} from "./shared/api";
+import {UserContext} from "./shared/context";
 
 function App() {
   return (
@@ -13,27 +15,22 @@ function App() {
         <TitledRoute title="Anmeldung" path="/login" component={LoginPage}/>
         <TitledRoute title="Registrierung" path="/register" component={RegisterPage}/>
 
-        <Route path="/a" component={AuthenticatedArea} />
+        <Route path="/a" component={AuthenticatedArea}/>
 
-        <Redirect to="/a" />
+        <Redirect to="/a"/>
       </Switch>
     </BrowserRouter>
   );
 }
 
 function AuthenticatedArea() {
-  const loggedIn = true; // TODO Use user state
   return (
-    <Switch>
-      {loggedIn ? (
-        <>
-          <TitledRoute title="Home" path="/a/home" component={HomePage}/>
-          <Redirect to="/a/home"/>
-        </>
-      ) : (
-        <Redirect to="/login"/>
-      )}
-    </Switch>
+    <api.Loader endpoint="/users" consumer={UserContext} createError={() => <Redirect to="/login" />}>
+      <Switch>
+        <TitledRoute title="Home" path="/a/home" component={HomePage}/>
+        <Redirect to="/a/home"/>
+      </Switch>
+    </api.Loader>
   )
 }
 
