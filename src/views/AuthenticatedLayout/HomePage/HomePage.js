7efa1740e3d379b47abddcd3eee5825e8a145ad6
Florian <privat@florian.ws>
2020-10-24 21:34:01 +0200

    Add event list item

--- src/views/AuthenticatedLayout/HomePage/HomePage.js
+++ src/views/AuthenticatedLayout/HomePage/HomePage.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import cls from './HomePage.module.less';
 import AuthenticatedLayout from "../AuthenticatedLayout";
-import {List, Avatar} from 'antd';
+import {Avatar, List} from 'antd';
+import {api} from "../../../shared/api";
+import UserAvatar from "../shared/UserAvatar/UserAvatar";
 
 export default () => {
-  const data = [ // TODO add real data
+  const [result] = api.useGet("/events")
+
+  return (
+    <AuthenticatedLayout title="Archery" contentClass={cls.content}>
+      <List
+        itemLayout="horizontal"
+        dataSource={result?.data?.["eventInfos"]}
+        renderItem={item => (<EventListItem item={item} />)}
+      />
+    </AuthenticatedLayout>
+  )
+}
+
+function EventListItem({item}) {
+  /* TODO Change display after list changes
+    const data = [
     {
       title: " seit 01.01.2020 13:10 ",
       description: "3 Pfeil Wertung - Runde 2 von 3"
@@ -14,26 +31,27 @@ export default () => {
       description: "3 Pfeil Wertung"
     },
   ];
+  */
+
+  const title = (
+    <>
+      <span className={cls.itemTitleParkour}>{item["parkour"]}</span>
+      {/*seit {item["timestamp"]}*/}
+    </>
+  )
+  const description = (
+    <>{item["gamemode"]} - Tier {item["animalCount"]} von {item["totalAnimals"]}</>
+  )
+
+  const getUserAvatar = info => (<UserAvatar username={info[0]} fullName={`${info[1]} ${info[2]}`} />);
 
   return (
-    <AuthenticatedLayout title="Archery" contentClass={cls.content}>
-      <List
-        itemLayout="horizontal"
-        dataSource={data}
-        renderItem={item => (
-          <List.Item className={cls.item}>
-            <List.Item.Meta
-              title={<><span className={cls.itemTitleParkour}>Parkour</span> {item.title}</>}
-              description={item.description}
-            />
-            <Avatar.Group maxCount={2} maxPopoverPlacement="bottom">
-              <Avatar size={"small"}>K</Avatar>
-              <Avatar size={"small"}>G</Avatar>
-              <Avatar size={"small"}>B</Avatar>
-            </Avatar.Group>
-          </List.Item>
-        )}
-      />
-    </AuthenticatedLayout>
+    <List.Item className={cls.item}>
+      <List.Item.Meta title={title} description={description}/>
+      <Avatar.Group maxCount={2} maxPopoverPlacement="bottom">
+        {getUserAvatar(item["creator"])}
+        {item["member"].map(memberInfo => getUserAvatar(memberInfo))}
+      </Avatar.Group>
+    </List.Item>
   )
 }
--- src/views/AuthenticatedLayout/shared/UserAvatar/UserAvatar.js
+++ src/views/AuthenticatedLayout/shared/UserAvatar/UserAvatar.js
@@ -1,8 +1,8 @@
 import React from "react";
-import {Avatar} from "antd";
+import {Tooltip, Avatar} from "antd";
 import cls from './UserAvatar.module.less';
 
-export default function ({username, ...rest}) {
+export default function ({username, fullName, ...rest}) {
   const colorClasses = [
     cls.avatarStyle1, cls.avatarStyle2, cls.avatarStyle3, cls.avatarStyle4, cls.avatarStyle5,
     cls.avatarStyle6, cls.avatarStyle7, cls.avatarStyle8, cls.avatarStyle9, cls.avatarStyle10
@@ -12,9 +12,15 @@ export default function ({username, ...rest}) {
     .map(char => char.charCodeAt(0))
     .reduce((total, current) => ((total << 5) - total) + current, 0));
 
-  return (
+  const avatar = (
     <Avatar {...rest} className={colorClasses[usernameSum % colorClasses.length]}>
       {usernameChar}
     </Avatar>
-  )
+  );
+
+  return fullName != null ? (
+    <Tooltip title={fullName} placement="bottom">
+      {avatar}
+    </Tooltip>
+  ) : avatar;
 }
