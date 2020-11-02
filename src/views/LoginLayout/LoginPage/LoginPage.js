9398023d79326b85343547ec4c9d9a092d203a07
Florian <privat@florian.ws>
2020-11-02 02:57:25 +0100

    Add favicon; Add credit links

--- src/views/LoginLayout/LoginPage/LoginPage.js
+++ src/views/LoginLayout/LoginPage/LoginPage.js
@@ -6,19 +6,27 @@ import {defaultRules} from "../../../shared/misc";
 import {FormError} from "../../../shared/FormError/FormError";
 import {useHistory} from "react-router-dom";
 import {api} from "../../../shared/api";
+import {showInModal} from "../../../shared/modal";
 
 export default () => {
-  return LoginLayout.wrap(
-    <Row align="middle" justify="center" className={cls.mainRow}>
-      <Col xs={20} md={8}>
-        <Typography.Title level={2} className={cls.title}>
-          Anmeldung
-        </Typography.Title>
+  return (
+    <LoginLayout>
+      <Row align="middle" justify="center" className={cls.mainRow}>
+        <Col xs={20} md={8}>
+          <Typography.Title level={2} className={cls.title}>
+            Anmeldung
+          </Typography.Title>
 
-        <LoginForm/>
-      </Col>
-    </Row>
-  );
+          <LoginForm/>
+        </Col>
+      </Row>
+
+      <Typography.Link onClick={() => showInModal(<Credits/>, {footer: null})}
+                       className={cls.creditsLink}>
+        Credits
+      </Typography.Link>
+    </LoginLayout>
+  )
 };
 
 const LoginForm = () => {
@@ -32,7 +40,8 @@ const LoginForm = () => {
 
   return (
     <Spin spinning={loading}>
-      <Form name="login" layout="vertical" onFinish={formData => api.put("/users/session", formData, handle).then(onResponse)}>
+      <Form name="login" layout="vertical"
+            onFinish={formData => api.put("/users/session", formData, handle).then(onResponse)}>
         <Form.Item
           className={cls.formItem}
           name="username"
@@ -49,7 +58,7 @@ const LoginForm = () => {
           <Input.Password placeholder="Passwort" size="large" autoComplete="current-password"/>
         </Form.Item>
 
-        <FormError message={result?.errorCode !== "ALREADY_LOGGED_IN" && result?.errorMessage} />
+        <FormError message={result?.errorCode !== "ALREADY_LOGGED_IN" && result?.errorMessage}/>
 
         <Form.Item>
           <Button type="primary" size="large" htmlType="submit" className={cls.submitButton}>
@@ -64,3 +73,12 @@ const LoginForm = () => {
     </Spin>
   );
 };
+
+function Credits() {
+  return (
+    <div>
+      Icons made by <a href="http://www.freepik.com/" title="Freepik">Freepik</a> {" "}
+      from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+    </div>
+  )
+}
