e1be987e431d69588b835eb6e285de2e7600cea6
Florian <privat@florian.ws>
2020-11-02 16:29:56 +0100

    Add message if user is not creator of ingame event

--- src/views/AuthenticatedLayout/EventPage/EventPage.js
+++ src/views/AuthenticatedLayout/EventPage/EventPage.js
@@ -3,20 +3,24 @@ import {api} from "../../../shared/api";
 import AuthenticatedLayout from "../AuthenticatedLayout";
 import IngamePage from "./IngamePage";
 import ResultPage from "./ResultPage";
+import {Empty} from "antd";
 
 export default function ({match: {params: {eventId}}}) {
   const [title, setTitle] = useState("");
-  // todo show notification if user is not creator
   return (
     <AuthenticatedLayout back="/a/home" title={title}>
       <api.Loader consumer={true} endpoint={`/events/${eventId}`}>
-        {({data}, reload) => (
-          <>
-            {data["eventIsFinished"]
-              ? (<ResultPage setTitle={setTitle} event={{eventId, ...data}}/>)
-              : (<IngamePage setTitle={setTitle} reload={reload} event={{eventId, ...data}}/>)}
-          </>
-        )}
+        {({data}, reload) => {
+          if (data["eventIsFinished"]) {
+            return (<ResultPage setTitle={setTitle} event={{eventId, ...data}}/>);
+          }
+
+          if (data["isCreator"]) {
+            return (<IngamePage setTitle={setTitle} reload={reload} event={{eventId, ...data}}/>);
+          }
+
+          return (<Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="Dieses Event ist noch nicht abgeschlossen"/>)
+        }}
       </api.Loader>
     </AuthenticatedLayout>
   )
